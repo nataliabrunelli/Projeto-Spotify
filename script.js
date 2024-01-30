63b7da09dfb07a9a5640ddd53da675c0e4cc20ed
Natalia <154284803+nataliabrunelli@users.noreply.github.com>
2024-01-30 00:40:31 -0300

    terminei o front

---
+++ script.js
@@ -0,0 +1,11 @@
+const greetingElement = document.getElementById("greeting");
+const currentHour = new Date().getHours();
+
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
+greetingElement.textContent = greetingMessage;
